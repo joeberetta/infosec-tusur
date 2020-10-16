0b8c473085b96a00eb1f41d70a4348b887cbb983
Joe Beretta <shakhrukh.97@gmail.com>
2020-10-16 20:34:51 +0700

    refactor: app compilation script

--- lab_1/src/caesar.ts
+++ lab_1/src/caesar.ts
@@ -12,79 +12,79 @@
  * console.log(cipher.decrypt("Jgnnq", 3)); // Hello
  */
 export class Caesar {
-  /**
-   * @constant Base dictionary
-   */
-  DICT = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-  /**
-   * @constructor
-   * @param {?string} dict - dictionary
-   */
-  constructor(dict = "") {
-    if (typeof dict === "string" && dict.length) {
-      this.DICT = dict.toUpperCase();
-    }
-  }
+	/**
+	 * @constant Base dictionary
+	 */
+	DICT = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
+	/**
+	 * @constructor
+	 * @param {?string} dict - dictionary
+	 */
+	constructor(dict = '') {
+		if (typeof dict === 'string' && dict.length) {
+			this.DICT = dict.toUpperCase();
+		}
+	}
 
-  /**
-   * Returns rotated letter
-   * @private
-   * @param letter
-   * @param rotation
-   */
-  __rotate(letter: string, rotation: number): string {
-    const letterIndex = this.DICT.indexOf(letter.toUpperCase());
+	/**
+	 * Returns rotated letter
+	 * @private
+	 * @param letter
+	 * @param rotation
+	 */
+	private rotate(letter: string, rotation: number): string {
+		const letterIndex = this.DICT.indexOf(letter.toUpperCase());
 
-    // Return source letter if not found in dictionary
-    if (letterIndex === -1) {
-      return letter;
-    }
+		// Return source letter if not found in dictionary
+		if (letterIndex === -1) {
+			return letter;
+		}
 
-    let rotationIndex = letterIndex + rotation;
+		let rotationIndex = letterIndex + rotation;
 
-    // Normalize rotation index if it's out of bound
-    if (rotationIndex >= this.DICT.length) {
-      const i = Math.floor(rotationIndex / this.DICT.length);
-      rotationIndex -= i * this.DICT.length;
-    } else if (rotationIndex < 0) {
-      const i = Math.floor(Math.abs(rotationIndex) / this.DICT.length);
-      rotationIndex = i || 1 * this.DICT.length + rotationIndex;
-    }
+		// Normalize rotation index if it's out of bound
+		if (rotationIndex >= this.DICT.length) {
+			const i = Math.floor(rotationIndex / this.DICT.length);
+			rotationIndex -= i * this.DICT.length;
+		} else if (rotationIndex < 0) {
+			const i = Math.floor(Math.abs(rotationIndex) / this.DICT.length);
+			rotationIndex = i || 1 * this.DICT.length + rotationIndex;
+		}
 
-    const rotatedLetter = this.DICT.charAt(Math.abs(rotationIndex));
+		const rotatedLetter = this.DICT.charAt(Math.abs(rotationIndex));
 
-    return letter === letter.toUpperCase()
-      ? rotatedLetter
-      : rotatedLetter.toLowerCase();
-  }
+		return letter === letter.toUpperCase()
+			? rotatedLetter
+			: rotatedLetter.toLowerCase();
+	}
 
-  /**
-   * Returns encrypted `text`
-   * @param {string} text to decrypt
-   * @param {number} shift symbols to rotate
-   */
-  encrypt(text: string, shift: number): string {
-    let result = "";
-    for (let i = 0; i < text.length; i++) {
-      const letter = text[i];
-      result += this.__rotate(letter, Math.abs(shift));
-    }
+	/**
+	 * Returns encrypted `text`
+	 * @param {string} text to decrypt
+	 * @param {number} shift symbols to rotate
+	 */
+	encrypt(text: string, shift: number): string {
+		let result = '';
+		for (let i = 0; i < text.length; i++) {
+			const letter = text[i];
+			result += this.rotate(letter, Math.abs(shift));
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  /**
-   * Returns decrypted `text`
-   * @param {string} text to decrypt
-   * @param {number} shift symbols to rotate
-   */
-  decrypt(text: string, shift: number): string {
-    let result = "";
-    for (let i = 0; i < text.length; i++) {
-      const letter = text[i];
-      result += this.__rotate(letter, -1 * Math.abs(shift));
-    }
+	/**
+	 * Returns decrypted `text`
+	 * @param {string} text to decrypt
+	 * @param {number} shift symbols to rotate
+	 */
+	decrypt(text: string, shift: number): string {
+		let result = '';
+		for (let i = 0; i < text.length; i++) {
+			const letter = text[i];
+			result += this.rotate(letter, -1 * Math.abs(shift));
+		}
 
-    return result;
-  }
+		return result;
+	}
 }
--- lab_1/src/index.ts
+++ lab_1/src/index.ts
@@ -1,40 +1,34 @@
-import { readFileSync, writeFileSync, existsSync } from "fs";
-import { Caesar } from "./caesar";
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { Caesar } from './caesar';
 
-const INPUT_FILE = "input.txt";
-const OUTPUT_FILE = "output.txt";
+const INPUT_FILE = 'input.txt';
+const OUTPUT_FILE = 'output.txt';
 
-const CRYPT_TYPE = process.argv[2] || "1";
+const CRYPT_TYPE = process.argv[2] || '1';
 const SHIFT = parseInt(process.argv[3]) || 3;
 const DICT = process.argv[4];
+
 const cipher = new Caesar(DICT);
 
-const close = (code: number) =>
-  setTimeout(() => {
-    process.exit(code || 1);
-  }, 3000);
-
 if (!existsSync(INPUT_FILE)) {
-  console.log("Файл не найден. Создайте input.txt файл!");
-  close(1);
+	console.log('Файл не найден. Создайте input.txt файл!');
+	process.exit(1);
 } else {
-  const sourceText = readFileSync(INPUT_FILE, { encoding: "utf8" });
-  const cryptedText =
-    CRYPT_TYPE == "1"
-      ? cipher.encrypt(sourceText, SHIFT)
-      : cipher.decrypt(sourceText, SHIFT);
+	const sourceText = readFileSync(INPUT_FILE, { encoding: 'utf8' });
+	const cryptedText =
+		CRYPT_TYPE == '1'
+			? cipher.encrypt(sourceText, SHIFT)
+			: cipher.decrypt(sourceText, SHIFT);
 
-  writeFileSync(OUTPUT_FILE, cryptedText, {
-    encoding: "utf8",
-  });
+	writeFileSync(OUTPUT_FILE, cryptedText, {
+		encoding: 'utf8',
+	});
 
-  console.log(`Алфавит:\t\t${cipher.DICT}`);
-  console.log(`Сдвиг:\t\t\t${SHIFT}`);
-  console.log(
-    `Тип:\t\t\t${CRYPT_TYPE == "1" ? "(1) Шифровать" : "(2) Расшифровать"}\n`
-  );
-  console.log(`Исходный текст:\t\t${sourceText}`);
-  console.log(`Зашифрованный текст:\t${cryptedText}`);
-
-  close(0);
+	console.log(
+		`[1] Тип:\t\t${CRYPT_TYPE == '1' ? '(1) Шифровать' : '(2) Расшифровать'}`
+	);
+	console.log(`[2] Сдвиг:\t\t${SHIFT}`);
+	console.log(`[3] Алфавит:\t\t${cipher.DICT}\n`);
+	console.log(`Исходный текст:\t\t${sourceText}`);
+	console.log(`Зашифрованный текст:\t${cryptedText}`);
 }
